fba6014d138f9bdd9f44f356523fa3999bac0f71
MELISOV <nd942gxfsj@privaterelay.appleid.com>
2026-01-13 23:31:38 +0500

    Add files via upload

---
+++ live/ver.php
@@ -0,0 +1,19 @@
+{
+  "appstore_url": "1",
+  "billboard_msg": "MLS",
+  "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
+  "client_ip": "",
+  "code": 0,
+  "country_code": "RU",
+  "force_to_restart_app": false,
+  "gdpr_version": 2,
+  "is_firewall_open": false,
+  "is_review_server": false,
+  "is_server_open": true,
+  "maintenance_announcement": "",
+  "maintenance_region": "",
+  "query_params": [],
+  "remote_option_version": "1.0.0",
+  "remote_version": "1.61.1",
+  "server_url": "https://api.frrrfrrrmobiles.ru/"
+}
